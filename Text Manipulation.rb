0683be322a8b2749f6bf971846e75597e4a43ade
Adam Louis <adamlouis@gmail.com>
2014-07-02 19:40:20 -0700

    Conforming to blatantly misrepresented spec

--- Text Manipulation.rb
+++ Text Manipulation.rb
@@ -1,1 +1,1 @@
-puts gets.gsub('Java', 'PHP').split(' ').map(&:capitalize).join(' ').split('.').map(&:strip).sort.join('.<br/>') << '.'
+puts gets.gsub(/java/i, 'PHP').split(' ').map(&:capitalize).join(' ').split('.').map(&:strip).sort.join('.<br/>') << '.'
